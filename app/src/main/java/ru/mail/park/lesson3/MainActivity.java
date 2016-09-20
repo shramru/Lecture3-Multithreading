161f8255bbeb8cad63bc5f7ff61409dfe143fa50
shramru <shramru@gmail.com>
2016-09-20 18:11:33 +0300

    homework 2. Constant fields

--- app/src/main/java/ru/mail/park/lesson3/MainActivity.java
+++ app/src/main/java/ru/mail/park/lesson3/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String URL_1 = "https://gist.githubusercontent.com/anonymous/66e735b3894c5e534f2cf381c8e3165e/raw/8c16d9ec5de0632b2b5dc3e5c114d92f3128561a/gistfile1.txt";
     private static final String URL_2 = "https://gist.githubusercontent.com/anonymous/be76b41ddf012b761c15a56d92affeb6/raw/bb1d4f849cb79264b53a9760fe428bbe26851849/gistfile1.txt";
+    private static final String textKey1 = "text1";
+    private static final String textKey2 = "text2";
 
     static {
         StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
@@ -40,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         text1 = (TextView) findViewById(R.id.text1);
         text2 = (TextView) findViewById(R.id.text2);
 
-        text1.setText("Click me");
-        text2.setText("Click me");
+        text1.setText(getString(R.string.click_me));
+        text2.setText(getString(R.string.click_me));
 
         text1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,13 +68,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadFromUrl(String url) {
-        textViewForUrl(url).setText("Loading...");
+        textViewForUrl(url).setText(getString(R.string.loading));
         UrlDownloader.getInstance().load(url);
     }
 
     private void onTextLoaded(String url, String stringFromUrl) {
         if (stringFromUrl == null) {
-            stringFromUrl = "Data unavailable";
+            stringFromUrl = getString(R.string.data_unavailable);
         }
 
         Toast.makeText(getApplicationContext(), stringFromUrl, Toast.LENGTH_SHORT).show();
@@ -85,21 +87,21 @@ public class MainActivity extends AppCompatActivity {
         } else if (URL_2.equals(url)) {
             return text2;
         }
-        throw new IllegalArgumentException("Unknown url: " + url);
+        throw new IllegalArgumentException(getString(R.string.unknown_url) + url);
     }
 
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putCharSequence("text1", text1.getText());
-        outState.putCharSequence("text2", text2.getText());
+        outState.putCharSequence(textKey1, text1.getText());
+        outState.putCharSequence(textKey2, text2.getText());
     }
 
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
         if (savedInstanceState != null) {
-            text1.setText(savedInstanceState.getCharSequence("text1"));
-            text2.setText(savedInstanceState.getCharSequence("text2"));
+            text1.setText(savedInstanceState.getCharSequence(textKey1));
+            text2.setText(savedInstanceState.getCharSequence(textKey2));
         }
     }
 }
