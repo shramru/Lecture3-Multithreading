418a7722ab730b7a189fd80a81159d501318342d
shramru <shramru@gmail.com>
2016-09-20 20:47:26 +0300

    homework 2. fix for hidden activity

--- app/src/main/java/ru/mail/park/lesson3/MainActivity.java
+++ app/src/main/java/ru/mail/park/lesson3/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.mail.park.lesson3;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "MyPrefs";
 
     private static final String URL_1 = "https://gist.githubusercontent.com/anonymous/66e735b3894c5e534f2cf381c8e3165e/raw/8c16d9ec5de0632b2b5dc3e5c114d92f3128561a/gistfile1.txt";
     private static final String URL_2 = "https://gist.githubusercontent.com/anonymous/be76b41ddf012b761c15a56d92affeb6/raw/bb1d4f849cb79264b53a9760fe428bbe26851849/gistfile1.txt";
@@ -27,10 +30,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView text1;
     private TextView text2;
 
+    SharedPreferences settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settings = getSharedPreferences(PREFS_NAME, 0);
 
         findViewById(R.id.open_activity).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +74,16 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void saveString(String key, String value) {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    private String getString(String key) {
+        return settings.getString(key, null);
+    }
+
     private void loadFromUrl(String url) {
         textViewForUrl(url).setText(getString(R.string.loading));
         UrlDownloader.getInstance().load(url);
@@ -79,6 +96,9 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(), stringFromUrl, Toast.LENGTH_SHORT).show();
         textViewForUrl(url).setText(stringFromUrl);
+
+        saveString(textKey1, text1.getText().toString());
+        saveString(textKey2, text2.getText().toString());
     }
 
     private TextView textViewForUrl(String url) {
@@ -92,16 +112,15 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putCharSequence(textKey1, text1.getText());
-        outState.putCharSequence(textKey2, text2.getText());
+
+        saveString(textKey1, text1.getText().toString());
+        saveString(textKey2, text2.getText().toString());
     }
 
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        if (savedInstanceState != null) {
-            text1.setText(savedInstanceState.getCharSequence(textKey1));
-            text2.setText(savedInstanceState.getCharSequence(textKey2));
-        }
+        text1.setText(getString(textKey1));
+        text2.setText(getString(textKey2));
     }
 }
